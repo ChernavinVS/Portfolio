c4e5668bc5064ded2bcfff0d10693eb3cb5d24fa
ChernavinVS <vs.chernavin@gmail.com>
2025-06-04 15:01:05 +0400

    Описание как работает код

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,36 +1,43 @@
-import React, { useState, useEffect } from 'react';
+// Отображение списка постов полученных с сервера
+import React, { useState, useEffect } from 'react'; // State состояние компонента
+                                                    // Effect хук состояния например загрузки
 import axios from 'axios';
 
-function Posts({ onPostChange }) {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+function Posts({ onPostChange }) {                  //  Posts принимает пропс onPostChange при изменения поста
+  const [posts, setPosts] = useState([]);           // массив постов пустой изначально
+  const [loading, setLoading] = useState(true);     // true показывает индикатор загрузки при первой загрузки постов
 
-  const loadPosts = async () => {
+  const loadPosts = async () => {   // асинхронная функция загрузки постов try cath finaly
     try {
-      setLoading(true);
-      const response = await axios.get('http://localhost:5000/posts');
-      setPosts(response.data);
+      setLoading(true);  // показываем индикатор загрузки
+      const response = await axios.get('http://localhost:5000/posts'); // гет запрос о количестве постов
+                                                                       //  и сохраняем в респонс
+      setPosts(response.data); // сохраняем респонс в состояние сетпостс
+                               // data это основной ответ который сервер прислал
     } catch (err) {
-      console.error('Ошибка загрузки постов:', err);
+      console.error('Ошибка загрузки постов:', err); // если ошибка выводим в консоль
     } finally {
-      setLoading(false);
+      setLoading(false);  // скрыть индикатор загрузки независимо от результата выполнения
     }
   };
 
   useEffect(() => {
     loadPosts();
-  }, []);
+  }, []); // массив пустой, загрузка выполняется сразу при монтировании компонета
+          // useEffect появляется сразу как только вызывается LoadPosts
 
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:5000/posts/${id}`);
-      loadPosts();
-      onPostChange?.();
+      loadPosts(); // обновляем список после удаления
+      onPostChange?.(); // вызываем пропс который покажет список постов после удаления поста
     } catch (err) {
-      console.error('Ошибка удаления:', err);
+      console.error('Ошибка удаления:', err); // если ошибка то показать сообщение
     }
   };
-
+// Редактирование поста
+// В асинхронную функцию через промт это диалоговое окно, передается post для редактирования
+// если новый заголовок и пост отличаются от старых имен и текста поста то применить изменения
   const handleEdit = async (post) => {
   const newTitle = prompt('Редактировать заголовок', post.title);
   if (newTitle === null) return;
@@ -44,22 +51,22 @@ function Posts({ onPostChange }) {
       title: newTitle, // Обновляем заголовок
       body: newBody    // Обновляем содержимое
     });
-    loadPosts();
-    onPostChange?.();
+    loadPosts(); // показать индикатор загрузки
+    onPostChange?.();  // пропс с перечнем постов после редактирования
   } catch (err) {
     console.error('Ошибка редактирования:', err);
   }
 };
 
   if (loading) return <p>Загрузка постов...</p>;
-  if (posts.length === 0) return <p>Нет постов для отображения</p>;
+  if (posts.length === 0) return <p>Нет постов для отображения</p>;//posts.length JS проверка массива posts
 
   return (
     <div style={{ maxWidth: 'auto', margin: '0 auto' }}>
       <h2 style={{ textAlign: 'start', marginBottom: '20px' , marginLeft :'10px'}}>Список постов</h2>
-      {posts.map(post => (
+      {posts.map(post => (//создает новый массив posts из элементов массива post, key как идентификатор каждого поста по id
         <div 
-          key={post.id}
+          key={post.id}  
           style={{
             border: '1px solid #ddd',
             
